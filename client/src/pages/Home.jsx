05d8a26f0bc00da4c6c7475245c069b5686e0f4c
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-02-26 12:44:13 +0530

    Styled Home page with Tailwind

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 const Home = () => {
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Welcome to Our Pharmacy</h1>
-      <p>Trusted medicines and healthcare products.</p>
+     <div className="max-w-6xl mx-auto py-20 px-6 text-center">
+      <h1 className="text-4xl font-bold text-blue-700 mb-6">
+        Welcome to PharmaCare
+      </h1>
+      <p className="text-gray-600 text-lg">
+        Your trusted online pharmacy for quality medicines and healthcare products.
+      </p>
     </div>
   );
 };
